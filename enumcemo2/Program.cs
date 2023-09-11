6dc2ba8894a2a0b6702547e27f62893b225a259f
Oliver Brage <Brage.Oliver@hotmail.com>
2023-09-11 10:31:42 +0200

    added one more cw

--- enumcemo2/Program.cs
+++ enumcemo2/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("testing");
             Console.WriteLine("jhgejhegjegjh");
+            Console.WriteLine("testeteastest");
         }
     }
 }
